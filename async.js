3e7e5e3681be5fb9955074c1d8b560a9eda2ba5d
LibLogic <hdgknsn@gmail.com>
2019-01-30 16:58:58 +0000

    async request using jQuery

--- async.js
+++ async.js
@@ -1,14 +1,18 @@
 window.onload = function(){
 	
-	var http = new XMLHttpRequest();
+	// var http = new XMLHttpRequest();
 	
-	http.open('GET', 'data/tweets.json', true);
-	http.send();
+	// http.open('GET', 'data/tweets.json', true);
+	// http.send();
 
-	http.onreadystatechange = function(){
-			if(http.readyState == 4 && http.status == 200){
-				console.log(JSON.parse(http.response));
-			}
-	};
+	// http.onreadystatechange = function(){
+	// 		if(http.readyState == 4 && http.status == 200){
+	// 			// console.log(JSON.parse(http.response));
+	// 		}
+	// };
+	
+	$.get("data/tweets.json", function(data){
+		console.log(data);
+	});
 	
 };
---
+++ version-2/jquery.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	
+	$.get("data/tweets.json", function(data){
+		console.log(data);
+	});
+	
+};
